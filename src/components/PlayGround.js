f963aa42cddbb27af7a2a7de5a20300d89917dfb
Sharad Dabhi <dabhisharad13@gmail.com>
2021-02-28 21:03:22 +0530

    Added facemesh to playground

--- src/components/PlayGround.js
+++ src/components/PlayGround.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
 import { connect } from "react-redux";
@@ -6,59 +6,95 @@ import SnakeGame from "../Games/Snake/SnakeGame";
 import { movement } from "../actions";
 import Test from "../Games/PAC-MAN/Test";
 import history from "./History";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import { drawMesh } from "./utilities";
+
 var twc = 28,
   x = 0,
   statusbol = 0;
-
+var net;
 const PredictWebCam = (props) => {
+  const webcamRef = useRef(null);
+  // const canvasRef = useRef(null);
   const [snakeComponentCalled, setSnakeComponentCalled] = useState(false);
   const [testComponentCalled, setTestComponentCalled] = useState(false);
 
   useEffect(() => {
+    runFacemesh();
+
     x = document.getElementById("headerdiv").clientHeight;
     twc = document.getElementById("twc").clientWidth;
 
     if (props.userId == "" || props.userId == "null") {
-      history.push("/games-ic/");
+      // history.push("/games-ic/");
     }
   }, []);
 
-  const webcamRef = React.useRef(null);
-
-  const setStatus = (predictedVal) => {
-    if (statusbol) {
-      if (predictedVal == 0) {
-        props.movement("DOWN");
-        // document.getElementById("status").innerHTML = "DOWN";
-      } else if (predictedVal == 1) {
-        props.movement("RIGHT");
-        // document.getElementById("status").innerHTML = "RIGHT";
-      } else if (predictedVal == 2) {
-        props.movement("LEFT");
-        // document.getElementById("status").innerHTML = "LEFT";
-      } else {
-        props.movement("UP");
-        // document.getElementById("status").innerHTML = "UP";
-      }
-      setTimeout(webcapture, 0);
-    }
+  const runFacemesh = async () => {
+    // NEW MODEL
+    net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh, {
+      maxFaces: 1,
+    });
+    console.log("MODEL LOADED");
   };
 
-  const webcapture = React.useCallback(() => {
-    try {
-      const imageSrc = webcamRef.current.getScreenshot().split(",").pop();
-      axios
-        .post("https://15.207.67.182:5000/predict", {
-          imageSrc: imageSrc,
-          idd: props.userId,
-        })
-        .then((res) => setStatus(res.data.Predicted));
-    } catch (err) {}
-  }, [webcamRef]);
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+
+      webcamRef.current.video.width = twc - 28;
+
+      const face = await net.estimateFaces({ input: video });
+
+      requestAnimationFrame(() => {
+        drawMesh(face);
+      });
+    }
+  };
+  //face mesh end
+
+  // const setStatus = (predictedVal) => {
+  //   if (statusbol) {
+  //     if (predictedVal == 0) {
+  //       props.movement("DOWN");
+  //       // document.getElementById("status").innerHTML = "DOWN";
+  //     } else if (predictedVal == 1) {
+  //       props.movement("RIGHT");
+  //       // document.getElementById("status").innerHTML = "RIGHT";
+  //     } else if (predictedVal == 2) {
+  //       props.movement("LEFT");
+  //       // document.getElementById("status").innerHTML = "LEFT";
+  //     } else {
+  //       props.movement("UP");
+  //       // document.getElementById("status").innerHTML = "UP";
+  //     }
+  //     // setTimeout(webcapture, 0);
+  //   }
+  // };
+
+  // const webcapture = React.useCallback(() => {
+  //   try {
+  //     const imageSrc = webcamRef.current.getScreenshot().split(",").pop();
+  //     axios
+  //       .post("https://15.207.67.182:5000/predict", {
+  //         imageSrc: imageSrc,
+  //         idd: props.userId,
+  //       })
+  //       .then((res) => setStatus(res.data.Predicted));
+  //   } catch (err) {}
+  // }, [webcamRef]);
 
   const startCalling = () => {
     statusbol = 1;
-    webcapture();
+    setInterval(() => {
+      detect(net);
+    }, 1000);
   };
 
   const stopCalling = () => {
@@ -99,12 +135,21 @@ const PredictWebCam = (props) => {
             </a>
           </div>
           <div>
-            <Webcam
-              audio={false}
-              ref={webcamRef}
-              screenshotFormat="image/jpg"
-              width={twc - 28}
-            />
+            <Webcam audio={false} mirrored ref={webcamRef} width={twc - 28} />
+            {/* <canvas
+              ref={canvasRef}
+              style={{
+                position: "absolute",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex: 9,
+                width: 640,
+                height: 480,
+              }}
+            /> */}
           </div>
         </div>
         <div id="gamediv" className="thirteen wide column">
